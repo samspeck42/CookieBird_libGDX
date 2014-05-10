93c087b640c560556f6e0b21ea872ac3168dc528
samspeck42 <samspeck42@gmail.com>
2014-05-10 18:53:02 -0500

    let's do this

---
+++ html/src/com/samspeck/cookiebird/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.samspeck.cookiebird.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.samspeck.cookiebird.CookieBirdGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new CookieBirdGame();
+        }
+}
